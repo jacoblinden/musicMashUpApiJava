6886e3051efb19b4e5ef65fa85bbb698cfa882ce
jacob <jacoblinden@hotmail.com>
2021-08-16 21:03:19 +0200

    first commit

---
+++ src/main/java/com/mechMusicAPI/demo/MechMusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.mechMusicAPI.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechMusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechMusicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mechMusicAPI/demo/MechMusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mechMusicAPI.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechMusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
